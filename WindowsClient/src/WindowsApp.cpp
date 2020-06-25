4273a2ae837ee13ca3a63adf1045688704f68c8d
Matthew Jones <jonesmat@usc.edu>
2020-06-24 23:56:34 -0700

    More boilerplate

--- WindowsClient/src/WindowsApp.cpp
+++ WindowsClient/src/WindowsApp.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include "HelloWorld.h"
 
 int main(int argc, char* argv[])
 {
-	std::cout << HelloWorld::Print() << std::endl;
+	std::cout << "Game Boy Emulator" << std::endl;
 	return 0;
 }
