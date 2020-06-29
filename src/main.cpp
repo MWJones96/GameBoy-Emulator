484daf8ff2c8549884f759d4d207e122a1c57b58
Matthew Jones <jonesmat@usc.edu>
2020-06-28 20:44:22 -0700

    Remove unused code from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include "sample_lib_1.hpp"
-#include "sample_lib_2.hpp"
 
-using namespace std;
-using namespace google_test_sample;
 int main(int argc, char *argv[])
 {
     if (argc != 2) 
