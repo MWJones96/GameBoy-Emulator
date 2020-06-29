66d0243f3c5585557943629a437caca71c74f627
Matthew Jones <jonesmat@usc.edu>
2020-06-28 20:43:22 -0700

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,14 @@ int main(int argc, char *argv[])
 {
     if (argc != 2) 
     {
+        //Program should take one and only one argument, which is the path of a Game Boy ROM file
         std::cerr << "ERROR: Invalid command-line format" << std::endl;
         return 1;
     }
 
-    auto sampleLib1 = SampleLib1();
-    auto sampleLib2 = SampleLib2();
+    char* romFilePath = argv[1];
+    std::cout << romFilePath << std::endl;
+
     std::cout << "Game Boy Emulator Starting Point" << std::endl;
 
     return 0;
