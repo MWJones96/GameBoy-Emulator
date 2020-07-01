a8e3d29e6be1d453b8ab2d0d04e4f17ec813b1f7
Matthew Jones <jonesmat@usc.edu>
2020-06-30 17:45:21 -0700

    More complex functionality

---
+++ CPU/src/CPU/lib.cpp
@@ -0,0 +1,6 @@
+#include "lib.hpp"
+
+int CPU::sumNumbers(int a, int b)
+{
+    return a + b;
+}
--- emulator/main.cpp
+++ emulator/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <cpu/lib.hpp>
 
 int main()
 {
     std::cout << "Game Boy Emulator Starting Point" << std::endl;
+
+    int sum = CPU::sumNumbers(10, 7);
+    std::cout << sum << std::endl;
+
     return 0;
 }
